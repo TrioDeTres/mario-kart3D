c112612ae94bbd4f1f075ff48c00a50c0fe80e69
naomijub <jnboeira@outlook.com>
2016-10-09 05:37:37 -0300

    Added Canvas

---
+++ MarioKart3D/Assets/Scripts/SpeedScrHUD.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpeedScrHUD : MonoBehaviour {
+    public GameObject kart;
+
+    private float speed;
+    Text text;
+
+	// Use this for initialization
+	void Awake () {
+        text = GetComponent<Text>();
+        speed = kart.GetComponent<Rigidbody>().velocity.magnitude;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //Due to wheel collider speed is 0
+        text.text = "Speed: " + speed;
+	}
+}
