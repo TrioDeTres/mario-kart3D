342679355108dc14fa98df22b4d8899e13923e22
naomijub <jnboeira@outlook.com>
2016-10-10 02:06:02 -0300

    Coins

---
+++ MarioKart3D/Assets/Scripts/CoinManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(0, 40 * Time.deltaTime, 0);
+	}
+
+    void OnTriggerEnter(Collider col) {
+        col.gameObject.GetComponent<KartCointPoints>().coins += 1;
+        GetComponentInChildren<MeshRenderer>().enabled = false;
+        AppearCoin();
+        GetComponentInChildren<MeshRenderer>().enabled = true;
+        Debug.Log(col.gameObject.GetComponent<KartCointPoints>().coins);
+    }
+
+    IEnumerator AppearCoin()
+    {
+        GetComponent<Collider>().enabled = false;
+        yield return new WaitForSeconds(3.5f);
+        GetComponent<Collider>().enabled = false;
+    }
+}
