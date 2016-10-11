36f6d58245d1a2b8a170609d346358848379a85e
naomijub <jnboeira@outlook.com>
2016-10-11 16:21:49 -0300

    Merge julia/pablo- terrain fix, timer and shell objects

--- MarioKart3D/Assets/Scripts/Kart/TriggerFlag.cs
+++ MarioKart3D/Assets/Scripts/Kart/TriggerFlag.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,12 +7,35 @@ public class TriggerFlag : MonoBehaviour {
     public int maxFlagValue;
     public static int currentFlagValue = 0;
     public static int laps = 0;
-	
+    public static string time;
+
+    float startTime;
+
+    void Start() {
+        startTime = Time.time;
+    }
+
 	// Update is called once per frame
 	void Update () {
         if (currentFlagValue != 0 && currentFlagValue >= maxFlagValue) {
             currentFlagValue = 0;
             laps++;
+            resetTime();
         }
+        setTime();
 	}
+
+    private void setTime()
+    {
+        float timeDiff = Time.time - startTime;
+        int min = (int)(Mathf.FloorToInt(timeDiff) / 60);
+        int sec = Mathf.FloorToInt(timeDiff) % 60;
+        int milisec = (int)((timeDiff - Mathf.Floor(timeDiff)) * 100);
+        time = "   " + min + "' " + sec + "'' " + milisec;
+    }
+
+    private void resetTime()
+    {
+        startTime = Time.time;
+    }
 }
