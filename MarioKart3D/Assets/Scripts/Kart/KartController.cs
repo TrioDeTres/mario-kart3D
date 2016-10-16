502dd32f774b797d8f5879984c4594999eab5b75
pablomparada <pablo.paradabol@gmail.com>
2016-10-16 19:58:37 -0200

    - Improved car controller script accordingly to friction and slip.

--- MarioKart3D/Assets/Scripts/Kart/KartController.cs
+++ MarioKart3D/Assets/Scripts/Kart/KartController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class KartController : MonoBehaviour
@@ -8,22 +9,36 @@ public class KartController : MonoBehaviour
     public float maxMotorTorque;
     public float maxSteeringAngle;
     public float maxSpeed = 60;
+    public float halfSpeed;
 
     public List<AudioSource> soundEffects;
 
     private new Rigidbody rigidbody;
     private bool isBraking;
     private Vector3 initialPosition;
-    private float revertTorqueForce;
+
+    private WheelFrictionCurve sidewayFriction;
+    private WheelFrictionCurve forwardFriction;
+    private JointSpring targetPosition;
+
+    private float timeAfterBrake;
+
+    [SerializeField]
+    private float speed;
 
     public void Start()
     {
         initialPosition = transform.position;
-        revertTorqueForce = maxMotorTorque * 2.0f;
 
         rigidbody = GetComponent<Rigidbody>();
 
         rigidbody.centerOfMass = centerOfMasss.localPosition;
+
+        halfSpeed =  maxSpeed / 2.0f;
+
+        sidewayFriction = wheels[0].sidewaysFriction;
+        forwardFriction = wheels[0].forwardFriction;
+        targetPosition = wheels[0].suspensionSpring;
 
         foreach (WheelCollider w in wheels)
         {
@@ -39,14 +54,19 @@ public class KartController : MonoBehaviour
         }
     }
 
+    public float CurrentSpeed()
+    {
+        float wheelCircunference = wheels[0].radius * 2 * Mathf.PI;
+        float rpm = (wheels[0].rpm + wheels[1].rpm) / 2.0f;
+        return wheelCircunference * rpm * 60.0f / 1000.0f;
+    }
+
     public void FixedUpdate()
     {
         float forward = maxMotorTorque * Input.GetAxis("Vertical");
         float steering = maxSteeringAngle * Input.GetAxis("Horizontal");
 
-        float speed = Vector3.Dot(rigidbody.velocity, transform.forward) * 3.6f;
-
-        Debug.Log(speed);
+        speed = CurrentSpeed();
 
         Brake();
 
@@ -61,17 +81,6 @@ public class KartController : MonoBehaviour
             wheels[3].motorTorque = forward;
 
         }
-        else if (speed > maxSpeed)
-        {
-            wheels[0].brakeTorque = revertTorqueForce;
-            wheels[1].brakeTorque = revertTorqueForce;
-            wheels[2].brakeTorque = revertTorqueForce;
-            wheels[3].brakeTorque = revertTorqueForce;
-            wheels[0].motorTorque = 0;
-            wheels[1].motorTorque = 0;
-            wheels[2].motorTorque = 0;
-            wheels[3].motorTorque = 0;
-        }
         else
         {
             wheels[0].motorTorque = 0;
@@ -80,6 +89,7 @@ public class KartController : MonoBehaviour
             wheels[3].motorTorque = 0;
         }
 
+        UpdateWheelFriction();
         PlaySounds(forward);
     }
 
@@ -89,17 +99,100 @@ public class KartController : MonoBehaviour
             isBraking = true;
             wheels[0].brakeTorque = 2500.0f;
             wheels[1].brakeTorque = 2500.0f;
-            wheels[2].brakeTorque = 2500.0f;
-            wheels[3].brakeTorque = 2500.0f;
         }
         else {
             isBraking = false;
             wheels[0].brakeTorque = 0.0f;
             wheels[1].brakeTorque = 0.0f;
-            wheels[2].brakeTorque = 0.0f;
-            wheels[3].brakeTorque = 0.0f;
         }
-        
+    }
+
+    private void UpdateWheelFriction()
+    {
+        float slipSpeed = halfSpeed + halfSpeed * 0.5f;
+
+        foreach (var w in wheels)
+        {
+            if (speed > slipSpeed)
+            {
+                WheelFrictionCurve forward = w.forwardFriction;
+
+                forward.extremumSlip = 1.3f;
+                forward.extremumValue = 2.0f;
+                forward.asymptoteSlip = 0.8f;
+                forward.asymptoteValue = 0.5f;
+                forward.stiffness = 0.7f;
+
+                w.forwardFriction = forward;
+
+                WheelFrictionCurve sideway = w.sidewaysFriction;
+
+                sideway.extremumSlip = 0.4f;
+                sideway.extremumValue = 1.0f;
+                sideway.asymptoteSlip = 0.5f;
+                sideway.asymptoteValue = 1.0f;
+                sideway.stiffness = 1.6f;
+
+                w.sidewaysFriction = sideway;
+
+                w.suspensionSpring = targetPosition;
+            }
+            else
+            {
+                w.forwardFriction = forwardFriction;
+                w.sidewaysFriction = sidewayFriction;
+                w.suspensionSpring = targetPosition;
+            }
+
+            
+        }
+
+        if (isBraking || timeAfterBrake > 0)
+        {
+            WheelCollider w = wheels[2];
+
+            WheelFrictionCurve forward = w.forwardFriction;
+            WheelFrictionCurve sideway = w.sidewaysFriction;
+
+            forward.stiffness = 1.2f;
+
+            sideway.extremumSlip = 1.0f;
+            sideway.extremumValue = 1.4f;
+            sideway.asymptoteSlip = 0.5f;
+            sideway.asymptoteValue = 0.9f;
+            sideway.stiffness = 1.5f;
+
+            JointSpring targetPosition = w.suspensionSpring;
+
+            targetPosition.targetPosition = 0.35f;
+
+            wheels[0].suspensionSpring = targetPosition;
+            wheels[0].sidewaysFriction = sideway;
+            wheels[0].forwardFriction = forward;
+
+            wheels[1].suspensionSpring = targetPosition;
+            wheels[1].sidewaysFriction = sideway;
+            wheels[1].forwardFriction = forward;
+
+            wheels[2].suspensionSpring = targetPosition;
+            wheels[2].sidewaysFriction = sideway;
+            wheels[2].forwardFriction = forward;
+
+            wheels[3].suspensionSpring = targetPosition;
+            wheels[3].sidewaysFriction = sideway;
+            wheels[3].forwardFriction = forward;
+
+            if (isBraking)
+            {
+                timeAfterBrake = 0.3f;
+            }
+            else
+            {
+                timeAfterBrake -= Time.deltaTime;
+            }
+        }
+
+        timeAfterBrake = Mathf.Clamp01(timeAfterBrake);
     }
 
     private void PlaySounds(float motor)
