828d329bedd06fe2275e99d8faca3387a23d775b
naomijub <jnboeira@outlook.com>
2016-10-25 21:57:22 -0200

    Yoshi Model

---
+++ MarioKart3D/Assets/Scripts/Objects/YoshiReferences.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class YoshiReferences : MonoBehaviour
+{
+    public SkinnedMeshRenderer bodyMesh;
+    public SkinnedMeshRenderer eyeMesh;
+
+    void Start()
+    {
+        bodyMesh.materials[0].SetTexture("_MainTex", Resources.Load<Texture2D>("Textures/Yoshi/Yoshi_Black_Body"));
+        eyeMesh.materials[0].SetTexture("_MainTex", Resources.Load<Texture2D>("Textures/Yoshi/Yoshi_Black_Eye"));
+        Debug.Log(bodyMesh.materials[0].mainTexture);
+    }
+}
